9137e3a00de8ecae251e6cc03d33c03a272c8de7
ashutosh <amxdp18@gmail.com>
2018-12-11 02:39:48 +0530

    story and comments added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  root 'welcome#index'
+ 
+   root 'stories#index'
+
+  resources :stories do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
